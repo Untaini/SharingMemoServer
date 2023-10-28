bbe2fabc25a4c81d86ee21f24121efa47df03714
Untaini <untaini@naver.com>
2023-10-29 02:49:45 +0900

    initial commit

---
+++ src/main/java/me/untaini/sharingmemo/SharingMemoApplication.java
@@ -0,0 +1,13 @@
+package me.untaini.sharingmemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharingMemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharingMemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/untaini/sharingmemo/SharingMemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.untaini.sharingmemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharingMemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
