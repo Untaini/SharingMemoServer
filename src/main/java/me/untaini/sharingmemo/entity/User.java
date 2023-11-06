4b744850642db6cd9ac30eeb4fcc56dfe76558f8
Untaini <untaini@naver.com>
2023-11-07 03:44:08 +0900

    feat(user): 유저 엔티티 생성
    - 유저 엔티티 생성
    - 유저 레포지토리 생성
    - 유저 DB 테스트 코드 작성
    
    Closes #4

---
+++ src/main/java/me/untaini/sharingmemo/entity/User.java
@@ -0,0 +1,26 @@
+package me.untaini.sharingmemo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "Users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String sid;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+}
