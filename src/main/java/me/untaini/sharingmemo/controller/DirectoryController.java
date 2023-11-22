d74334f49756f387a3cee47f68bd6fd5586e674a
Untaini <untaini@naver.com>
2023-11-22 10:16:33 +0900

    feat(directory): 디렉토리 컨트롤러 생성

---
+++ src/main/java/me/untaini/sharingmemo/controller/DirectoryController.java
@@ -0,0 +1,18 @@
+package me.untaini.sharingmemo.controller;
+
+import me.untaini.sharingmemo.service.DirectoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/directory")
+public class DirectoryController {
+
+    private final DirectoryService directoryService;
+
+    @Autowired
+    public DirectoryController(DirectoryService directoryService) {
+        this.directoryService = directoryService;
+    }
+}
