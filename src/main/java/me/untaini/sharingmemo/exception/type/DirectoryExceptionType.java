37a4804cb789b71016a8d8f357510e98284ba3ac
Untaini <untaini@naver.com>
2023-11-25 20:41:44 +0900

    feat(directory): 디렉토리가 같은 이름으로 변경을 요청할 때의 예외 타입 추가

--- src/main/java/me/untaini/sharingmemo/exception/type/DirectoryExceptionType.java
+++ src/main/java/me/untaini/sharingmemo/exception/type/DirectoryExceptionType.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum DirectoryExceptionType implements BaseExceptionType {
     NOT_FOUND(HttpStatus.NOT_FOUND, "해당 디렉토리를 찾을 수 없습니다."),
     OWNER_NOT_MATCH(HttpStatus.BAD_REQUEST, "디렉토리를 소유하지 않았습니다."),
-    EXIST_SAME_NAME(HttpStatus.CONFLICT, "같은 이름이 디렉토리에 있습니다.");
+    EXIST_SAME_NAME(HttpStatus.CONFLICT, "같은 이름이 디렉토리에 있습니다."),
+    ALREADY_HAS_SAME_NAME(HttpStatus.BAD_REQUEST, "현재 디렉토리는 이미 그 이름을 가지고 있습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
