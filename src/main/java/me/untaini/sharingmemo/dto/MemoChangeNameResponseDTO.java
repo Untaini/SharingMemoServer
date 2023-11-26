666fc33038814532e16a6bdb9fd2a70f3ebccaad
Untaini <untaini@naver.com>
2023-11-26 17:48:52 +0900

    feat(memo): 메모 이름 수정을 위한 DTO 생성

---
+++ src/main/java/me/untaini/sharingmemo/dto/MemoChangeNameResponseDTO.java
@@ -0,0 +1,14 @@
+package me.untaini.sharingmemo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class MemoChangeNameResponseDTO {
+
+    private String beforeName;
+
+}
