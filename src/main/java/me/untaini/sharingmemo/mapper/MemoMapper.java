c9f562d6f9d4c289a749818588d0cd01756d97fe
Untaini <untaini@naver.com>
2023-11-26 16:57:59 +0900

    fix(mapper): Spring Bean이 Mapper를 인식하지 못하는 버그 수정

--- src/main/java/me/untaini/sharingmemo/mapper/MemoMapper.java
+++ src/main/java/me/untaini/sharingmemo/mapper/MemoMapper.java
@@ -4,15 +4,18 @@ import me.untaini.sharingmemo.dto.MemoCreateRequestDTO;
 import me.untaini.sharingmemo.dto.MemoCreateResponseDTO;
 import me.untaini.sharingmemo.entity.Directory;
 import me.untaini.sharingmemo.entity.Memo;
+import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
+@Mapper
 public interface MemoMapper {
 
     MemoMapper INSTANCE = Mappers.getMapper(MemoMapper.class);
 
     @Mapping(source = "directory", target = "directory")
     @Mapping(source = "requestDTO.userId", target = "ownerId")
+    @Mapping(source = "requestDTO.name", target = "name")
     Memo DirectoryAndMemoCreateRequestDTOToMemo(Directory directory, MemoCreateRequestDTO requestDTO);
 
 
