5ac1459793d85eb53b3d629a1f198d0616f7468c
Untaini <untaini@naver.com>
2023-11-26 22:59:16 +0900

    fix(memo): 메모 레포지토리의 기본키 타입 변경
    메모 엔티티의 기본키 타입이 변경되었는데 레포지토리에 반영하지 않았던 버그 수정

--- src/main/java/me/untaini/sharingmemo/repository/MemoRepository.java
+++ src/main/java/me/untaini/sharingmemo/repository/MemoRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemoRepository extends JpaRepository<Memo, String> {
+public interface MemoRepository extends JpaRepository<Memo, Long> {
 
 }
