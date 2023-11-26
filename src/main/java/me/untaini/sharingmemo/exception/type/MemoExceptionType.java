b2e815195e3122c0cbfab4217da61ece23c97420
Untaini <untaini@naver.com>
2023-11-26 16:38:56 +0900

    feat(memo): 메모 이름이 중복될 시의 예외 처리를 위한 타입 추가

--- src/main/java/me/untaini/sharingmemo/exception/type/MemoExceptionType.java
+++ src/main/java/me/untaini/sharingmemo/exception/type/MemoExceptionType.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum MemoExceptionType implements BaseExceptionType {
+
+    EXIST_SAME_NAME(HttpStatus.CONFLICT, "같은 이름이 디렉토리에 있습니다.")
     ;
 
     private final HttpStatus httpStatus;
