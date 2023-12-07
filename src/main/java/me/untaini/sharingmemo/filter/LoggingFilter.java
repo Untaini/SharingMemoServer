dc96e83a23c370b506d6735e0250313de0210c99
Untaini <untaini@naver.com>
2023-12-07 23:00:29 +0900

    refactor(log): 사용하지 않는 import문, record 삭제

--- src/main/java/me/untaini/sharingmemo/filter/LoggingFilter.java
+++ src/main/java/me/untaini/sharingmemo/filter/LoggingFilter.java
@@ -4,16 +4,12 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.util.ContentCachingRequestWrapper;
-import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import java.io.IOException;
 
@@ -23,45 +19,13 @@ import java.io.IOException;
 public class LoggingFilter extends OncePerRequestFilter {
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NotNull  HttpServletRequest request,
+                                    @NotNull HttpServletResponse response,
+                                    @NotNull FilterChain filterChain) throws ServletException, IOException {
 
         filterChain.doFilter(request, response);
 
         log.info("|[REQUEST] {} {}", request.getMethod(), request.getRequestURI());
-
-        @Builder
-        record RequestLogMessage (
-                ContentCachingRequestWrapper requestWrapper
-        ) {
-
-            public String getLogMessage() {
-                String jsonBody = new String(requestWrapper.getContentAsByteArray());
-
-                return new StringBuilder()
-                        .append(String.format("\n|[REQUEST] %s %s\n", requestWrapper.getMethod(), requestWrapper.getRequestURI()))
-                        .append(String.format("|REQUEST_PARAM: %s\n", requestWrapper.getParameterMap()))
-                        .append(String.format("|REQUEST_BODY: %s\n", jsonBody))
-                        .toString();
-            }
-        }
-
-        @Builder
-        record ResponseLogMessage (
-                ContentCachingResponseWrapper responseWrapper
-        ) {
-
-            public String getLogMessage() throws IOException {
-                String jsonBody = new String(responseWrapper.getContentAsByteArray());
-                responseWrapper.copyBodyToResponse();
-
-                return new StringBuilder()
-                        .append(String.format("|[RESPONSE] %d %s\n", responseWrapper.getStatus(), HttpStatus.valueOf(responseWrapper.getStatus())))
-                        .append(String.format("|RESPONSE_BODY: %s\n", jsonBody))
-                        .toString();
-            }
-        }
         log.info("|[RESPONSE] {}", HttpStatus.valueOf(response.getStatus()));
     }
  }
